c1b001e7903dc6b890455ad974bb855d133bb7ef
Isaac Duong <isaacduong157@gmail.com>
2023-09-19 22:20:25 +1000

    Implement example project for LHS col

--- src/initial.js
+++ src/initial.js
@@ -1,24 +1,31 @@
+import Delete from './img/delete.png'
 export function generateLayout() {
     
     const container = document.querySelector('body');
 
-    const addProject = document.createElement('div');
-    const addTask = document.createElement('div');
-    const projName = document.createElement('div');
+    const projCol = document.createElement('div');
+    const taskCol = document.createElement('div');
+    const proj = document.createElement('div');
     const projDisplay = document.createElement('div');
+    const projName = document.createElement('div');
+    const remProj = document.createElement('img');
     const divider = document.createElement('div');
     const addProjBtn = document.createElement('div');
     const plusSymbol = document.createElement('p');
 
-    addTask.classList.add('addTask');
-    addProject.classList.add('addProject');
+    taskCol.classList.add('taskCol');
+    projCol.classList.add('projCol');
     divider.className = 'divider';
 
-    projName.textContent = 'Example Project';
+    proj.textContent = 'Projects';
     plusSymbol.textContent = '+'
     addProjBtn.textContent = 'Add Project';
 
-    container.append(addProject, addTask)
-    addProjBtn.append(plusSymbol)
-    addProject.append(projName, divider, projDisplay, addProjBtn);
+    projName.textContent = 'Example Project';
+    remProj.src = Delete;
+
+    container.append(projCol, taskCol);
+    addProjBtn.append(plusSymbol);
+    projDisplay.append(projName, remProj);
+    projCol.append(proj, divider, projDisplay, addProjBtn);
 }
